aeda433e1b7ccf86c0c4106756311bc21a8984df
Takuya <dyhpaax@gmail.com>
2021-10-02 16:40:50 +0900

    Add:バリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase} # DBに保存する前にemail属性を強制的に小文字に変換する
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+end
