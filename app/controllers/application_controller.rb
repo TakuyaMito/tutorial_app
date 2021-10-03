baedfcdaa478d171867dfe5a758832d0c8ff5a8c
Takuya <dyhpaax@gmail.com>
2021-10-04 00:30:43 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+  include SessionsHelper
 
   def hello
     render html: "hello, world!"
