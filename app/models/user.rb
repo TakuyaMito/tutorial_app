e6cfcc7b8aacb0f98ad5762c969ca292ba62841e
Takuya <dyhpaax@gmail.com>
2021-10-04 12:31:46 +0900

    Add:永続セッションからのログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,13 +23,19 @@ class User < ApplicationRecord
    end
 
   #  永続セッションのためにユーザーをデータベースに記憶する
-  def remeber
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
   
   # 渡されたトークンが記憶ダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
